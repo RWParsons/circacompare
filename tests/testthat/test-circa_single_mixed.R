fe1bf73213bfa44cb74259883d271c123386d6ad
ATpoint <a.toenges@uni-muenster.de>
2023-11-04 14:28:53 +0100

    add tests for weights

--- tests/testthat/test-circa_single_mixed.R
+++ tests/testthat/test-circa_single_mixed.R
@@ -40,3 +40,70 @@ test_that("circa_single_mixed() works", {
   tau_ul <- tau_est + 1.96 * fit_tau["std_error"]
   expect_true(tau_in < tau_ul & tau_in > tau_ll)
 })
+
+### make test that weights are used correctly and malformatted weights are detected
+test_that("weights work", {
+
+  set.seed(42)
+  mixed_data <- function(n) {
+   counter <- 1
+   for (i in 1:n) {
+     x <- make_data(k1 = rnorm(1, 10, 2), alpha1 = 0, phi1 = 0)
+     x$id <- counter
+     counter <- counter + 1
+     if (i == 1) {
+       res <- x
+     } else {
+       res <- rbind(res, x)
+     }
+   }
+   return(res)
+  }
+
+  df <- mixed_data(n = 50)
+
+  # no weights used (= all weights are 1), hence fit$apVar should not be populated
+  out <- circa_single_mixed(
+   x = df, col_time = "time", col_outcome = "measure",
+   col_id = "id", randomeffects = c("k")
+  )
+  expect_true(is(out$fit$apVar, "character"))
+
+  # when weights are not all 1 then fit$apVar should be a matrix
+  sw <- runif(n=nrow(df))
+  out2 <- circa_single_mixed(
+    x = df, col_time = "time", col_outcome = "measure",
+    col_id = "id", randomeffects = c("k"), weights = sw
+  )
+  expect_true(is(out2$fit$apVar, "matrix"))
+
+  # weights must be same length as nrow(x)
+  sw2 <- c(sw, 1)
+  expect_error(
+    circa_single_mixed(
+      x = df, col_time = "time", col_outcome = "measure",
+      col_id = "id", randomeffects = c("k"), weights = sw2
+    )
+  )
+
+  # weights must not contain NA
+  sw3 <- sw
+  sw3[1] <- NA
+  expect_error(
+    circa_single_mixed(
+      x = df, col_time = "time", col_outcome = "measure",
+      col_id = "id", randomeffects = c("k"), weights = sw3
+    )
+  )
+
+  # weights must not be negative
+  sw4 <- sw
+  sw4[1] <- -1
+  expect_error(
+    circa_single_mixed(
+      x = df, col_time = "time", col_outcome = "measure",
+      col_id = "id", randomeffects = c("k"), weights = sw4
+    )
+  )
+
+})
--- tests/testthat/test-circacompare_mixed.R
+++ tests/testthat/test-circacompare_mixed.R
@@ -33,3 +33,99 @@ test_that("circacompare_mixed() works", {
   phi1_ul <- phi1_est + 1.96 * phi1_fit["std_error"]
   expect_true(phi1_in < phi1_ul & phi1_in > phi1_ll)
 })
+
+### make test that weights are used correctly and malformatted weights are detected
+test_that("weights work", {
+
+  set.seed(99)
+  phi1_in <- 3.15
+  mixed_data <- function(n) {
+    counter <- 1
+    for (i in 1:n) {
+      x <- make_data(k1 = 0, alpha1 = 0, phi1 = rnorm(1, phi1_in, 0.5), hours = 72, noise_sd = 1)
+      x$id <- counter
+      counter <- counter + 1
+      if (i == 1) {
+        res <- x
+      } else {
+        res <- rbind(res, x)
+      }
+    }
+    return(res)
+  }
+
+  df <- mixed_data(20)
+
+  out <- circacompare_mixed(
+    x = df,
+    col_time = "time",
+    col_group = "group",
+    col_outcome = "measure",
+    col_id = "id",
+    control = list(grouped_params = c("phi"), random_params = c("phi1"))
+  )
+
+  # no weights used (= all weights are 1), hence fit$apVar should not be populated
+  out <- circacompare_mixed(
+   x = df,
+   col_time = "time",
+   col_group = "group",
+   col_outcome = "measure",
+   col_id = "id",
+   control = list(grouped_params = c("phi"), random_params = c("phi1"))
+  )
+  expect_true(is(out$fit$apVar, "character"))
+
+  # when weights are not all 1 then fit$apVar should be a matrix
+  sw <- runif(n=nrow(df))
+  out2 <- circacompare_mixed(
+    x = df,
+    col_time = "time",
+    col_group = "group",
+    col_outcome = "measure",
+    col_id = "id",
+    control = list(grouped_params = c("phi"), random_params = c("phi1")),
+    weights = sw
+  )
+  expect_true(is(out2$fit$apVar, "matrix"))
+
+  # weights must be same length as nrow(x)
+  sw2 <- c(sw, 1)
+  expect_error(
+    circa_single_mixed(
+      x = df, col_time = "time", col_outcome = "measure",
+      col_id = "id", randomeffects = c("k"), weights = sw2
+    )
+  )
+
+  # weights must not contain NA
+  sw3 <- sw
+  sw3[1] <- NA
+  expect_error(
+    circacompare_mixed(
+      x = df,
+      col_time = "time",
+      col_group = "group",
+      col_outcome = "measure",
+      col_id = "id",
+      control = list(grouped_params = c("phi"), random_params = c("phi1")),
+      weights = sw3
+    )
+  )
+
+  # weights must not be negative
+  sw4 <- sw
+  sw4[1] <- -1
+  expect_error(
+    circacompare_mixed(
+      x = df,
+      col_time = "time",
+      col_group = "group",
+      col_outcome = "measure",
+      col_id = "id",
+      control = list(grouped_params = c("phi"), random_params = c("phi1")),
+      weights = sw4
+    )
+  )
+
+})
